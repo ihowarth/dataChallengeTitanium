e38223c8ff99e73531a42243c499d44a66e5c977
Ian Howarth <ianhowarth@Ians-MacBook-Pro.local>
2014-09-22 23:53:03 -0500

    Added UI, with event listeners ready to call the functions and pass values. Challenge 5 (deletebyemail) is complete and working

--- app/alloy.js
+++ app/alloy.js
@@ -2,3 +2,11 @@ var NameDatabase    = require( "nameDatabase"    );
 var EmailGenerator  = require( "emailGenerator"  );
 var MemberPrototype = require( "memberPrototype" );
 var DataSetCreator  = require( "dataSetCreator"  );
+
+
+var APP = {
+    validateEmail : function( email ) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+    }
+};
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -16,8 +16,6 @@ var rootMember = DataSetCreator.createDataSet([
 	0	
 ]);
 
-var level = 1;
-
 
 (function init() {
 	addEventListeners();
@@ -27,9 +25,44 @@ var level = 1;
 
 
 function addEventListeners() {
-	$.mainWin.addEventListener( "click" , function() {
-		printSocialGraph( rootMember );
+	$.printLevelsButton.addEventListener( "click" , function() {
+	    if ( $.searchNamesTextField.value != "" ) {
+	       printSocialGraph( rootMember );
+	       
+	    } else {
+	       alert( "Please enter a level to print" ); 
+	    }
 	});
+	
+	
+	$.searchFriendByEmailButton.addEventListener( "click" , function() {
+        if ( APP.validateEmail( $.searchEmailTextField.value ) ) {
+            searchFriendByEmail( rootMember , $.searchEmailTextField.value.toLowerCase() );    
+            
+        } else {
+            alert( "Please enter a real e-mail address" );
+        }
+    });
+    
+    
+    $.searchExtendedFriendsByName.addEventListener( "click" , function() {
+        if ( $.searchNamesTextField.value != "" ) {
+            searchForExtendedFriends( rootMember , $.searchNamesTextField.value.toLowerCase() );    
+            
+        } else {
+            alert( "Please enter a name" );
+        }
+    });
+    
+    
+    $.deleteFriendByEmailButton.addEventListener( "click" , function() {
+        if ( APP.validateEmail( $.deleteEmailTextField.value ) ) {
+            deleteFriendByEmail( rootMember , $.deleteEmailTextField.value.toLowerCase() );    
+            
+        } else {
+            alert( "Please enter a real e-mail address" );
+        }
+    });
 };
 
 
@@ -39,7 +72,10 @@ function addEventListeners() {
  * Challenge 2
  * Add a second parameter to the function to specify which level to print and only print
  */  
-function printSocialGraph( member , levelToPrint ) {
+function printSocialGraph( member ) {
+    // Selected level to print, 0 means print all levels in order
+    var levelToPrint = $.selectLevelTextField.value;
+    
 	console.log( JSON.stringify( member ) );
 };
 
@@ -58,27 +94,55 @@ function printSocialGraph( member , levelToPrint ) {
 /* Challenge 3
  * Return the name and the level where you find the friend
  */
-function searchFriendByEmail( email ) {
-	
+function searchFriendByEmail( member , email ) {
+    
 };
+
 
 
 /* Challenge 4
  * Return an array of extended friends between the 2nd and 4th level, 
  * using the given name as the search parameter
  */
-function searchForExtendedFriends( name ) {
+function searchForExtendedFriends( member , name ) {
 	
 };
+
+
+
 
 
 /* Challenge 5
  * Delete a friend from the given member object
  */
-function deleteFriendByEmail( email ) {
-	for ( var i = 0; i < rootMember.listOfFriends; i++ ) {
-		if ( rootMember.listOfFriends[i].email == email ) {
-			rootMember.listOfFriends.slice( i , 1 );
-		}
-	} 
+function deleteFriendByEmail( member , email ) {
+    console.log( "\\\\\ BEFORE DELETE /////" );
+    console.log( JSON.stringify( member ));
+    
+    // Loop through all the first level friends of the rootMember
+    if ( member.listOfFriends.length > 0 ) {
+    	for ( var i = 0; i < member.listOfFriends.length; i++ ) {
+    	    // If the email addresses match, delete it from the array
+    		if ( member.listOfFriends[i].email.toLowerCase() == email ) {
+    			member.listOfFriends.splice( i , 1 );
+    			
+    			alert( "Friend deleted, please check console to confirm" );
+    			
+    			console.log( "\\\\\ AFTER DELETE /////" );
+    			console.log( JSON.stringify( member ));
+    			
+    			// Break out after we find a match, as emails are unique
+    			break;
+    		}
+    		
+    		// If you reach the end of the list without finding a matching friend, alert the user
+    		if ( (i + 1) == member.listOfFriends.length ) {
+    		    alert( "No friends found with that email address" );
+    		}
+    	}
+    	
+    // If there are no friends, alert the user   
+    } else {
+        alert( "This user has no friends :(" );
+    }
 };
